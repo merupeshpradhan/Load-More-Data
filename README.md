fa8703bb78a23647854649a786c6a0ce0a444874
Rupesh pradhan <merupeshpradhan@gmail.com>
2025-07-24 11:52:03 +0530

    Project fully complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Frontend project Load-MOre-Data
+
+### free public API
+
+```
+https://dummyjson.com/
+```
--- src/load-more-data/index.jsx
+++ src/load-more-data/index.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
+import "./styles.css";
 
 export default function LoadMoreData() {
   const [loading, setLoading] = useState(false);
-  const [pruducts, setPruducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [count, setCount] = useState(0);
+  const [disabaleButton, setDisabaleButton] = useState(false);
 
   async function fetchProducts() {
     try {
+      setLoading(true);
       const response = await fetch(
         `https://dummyjson.com/products?limit=20&skip=${
           count === 0 ? 0 : count * 20
@@ -14,15 +17,49 @@ export default function LoadMoreData() {
       );
 
       const result = await response.json();
+
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+
       console.log(result);
     } catch (e) {
       console.log(e);
+      setLoading(false);
     }
   }
 
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [count]);
 
-  return <div className="container">Container</div>;
+  useEffect(() => {
+    if (products && products.length === 180) setDisabaleButton(true);
+  }, [products]);
+
+  if (loading) {
+    return <div>Loading data ! please wait.</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div className="product" key={item.id}>
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button disabled={disabaleButton} onClick={() => setCount(count + 1)}>
+          Load more Products
+        </button>
+        {disabaleButton ? <p>You have reached to 194 products</p> : null}
+      </div>
+    </div>
+  );
 }
